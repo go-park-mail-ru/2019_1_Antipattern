4173c516ffa96b8d0adb71f60494b75904f06ab6
Vladimir <ukhachev.v.s@gmail.com>
2019-03-04 15:24:32 +0300

    Session middleware

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 )
 
 type User struct {
-	uuid          string
+	uuid          uint32
 	login         string
 	password_hash string
 	email         string
@@ -18,27 +18,39 @@ type User struct {
 }
 
 type Session struct {
-	user User
+	sid  string
+	user *User
+}
+
+func (session *Session) Save() error {
+	sessions[session.sid] = *session
+	return nil
+}
+func (user *User) Save() error {
+	// TODO: Save to db logic
+	return nil
 }
 
 var users map[string]User
 var sessions map[string]Session
 
-func createSession(user *User) string {
-	session := Session{
-		user: *user,
-	}
+func NewSession() *Session {
 	id := uuid.New().String()
+	session := Session{
+		sid:  id,
+		user: nil,
+	}
+
 	sessions[id] = session
-	return id
+	return &session
 }
 
-func Register(login string, password string, email string, name string) (string, error) {
-
+func NewUser(login string, password string, email string, name string) (*User, error) {
 	if _, ok := users[login]; ok {
-		return "", errors.New("User already exists " + login)
+		return nil, errors.New("User already exists " + login)
 	}
 	user := User{
+		uuid:          uuid.New().ID(),
 		login:         login,
 		password_hash: password,
 		email:         email,
@@ -46,59 +58,84 @@ func Register(login string, password string, email string, name string) (string,
 	}
 
 	users[login] = user
-	id := createSession(&user)
-	return id, nil
+	return &user, nil
 }
 
-func Auth(login string, password string) (string, error) {
+func Auth(login string, password string) (*User, error) {
 	user, ok := users[login]
 	if !ok {
-		return "", errors.New("User not exists " + login)
+		return nil, errors.New("User not exists " + login)
 	}
 	if user.password_hash != password {
-		return "", errors.New("Wrong password " + login)
+		return nil, errors.New("Wrong password " + login)
 	}
-	id := createSession(&user)
-	return id, nil
+
+	return &user, nil
 }
 
-func HandleAuth(w http.ResponseWriter, r *http.Request) {
+func HandleAuth(w http.ResponseWriter, r *http.Request, session *Session) {
 	login := r.FormValue("login")
 	password := r.FormValue("password")
 
-	cookie, err := r.Cookie("uuid")
-	// TODO: In case if cookie is broken user must auth
-	if err != nil {
-		id, err := Auth(login, password)
-		if err != nil {
-			fmt.Fprintln(w, err.Error())
-		} else {
-			cookie = &http.Cookie{Name: "uuid", Value: id, HttpOnly: true}
-			http.SetCookie(w, cookie)
-			fmt.Fprintln(w, "Succesfull", id)
-		}
-	} else {
+	if session.user != nil {
 		fmt.Fprintln(w, "Already logged in")
-	}
+	} else {
+		user, err := Auth(login, password)
+		if err != nil {
+			fmt.Fprintln(w, "Auth error", err.Error())
+		} else {
+			session.user = user
+			fmt.Fprintln(w, "Authorized", login, session.sid)
+		}
 
+	}
 }
 
-func HandleRegister(w http.ResponseWriter, r *http.Request) {
+// HandleRegister handle registration api request
+// request must contain post form:
+// 	login, password, email, name
+// Writes status json to response
+func HandleRegister(w http.ResponseWriter, r *http.Request, session *Session) {
 	login := r.FormValue("login")
 	password := r.FormValue("password")
 	email := r.FormValue("email")
 	name := r.FormValue("name")
 
-	id, err := Register(login, password, email, name)
+	user, err := NewUser(login, password, email, name)
 	if err == nil {
-		cookie := &http.Cookie{Name: "uuid", Value: id, HttpOnly: true}
-		http.SetCookie(w, cookie)
-
-		fmt.Fprintln(w, "User registered:", id)
+		session.user = user
+		fmt.Fprintln(w, "User registered:", user.login, session.sid)
 	} else {
 		fmt.Fprintln(w, err.Error())
 	}
+}
 
+func SessionMiddleware(next func(http.ResponseWriter, *http.Request, *Session)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("sid")
+		if err != nil {
+			session := NewSession()
+			cookie = &http.Cookie{
+				Name:     "sid",
+				Value:    session.sid,
+				HttpOnly: true,
+			}
+			http.SetCookie(w, cookie)
+		}
+		session, ok := sessions[cookie.Value]
+		if !ok {
+			session = *NewSession()
+			cookie = &http.Cookie{
+				Name:     "sid",
+				Value:    session.sid,
+				HttpOnly: true,
+			}
+			http.SetCookie(w, cookie)
+		}
+		next(w, r, &session)
+		session.Save()
+
+	}
 }
 
 func main() {
@@ -107,8 +144,8 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/auth", HandleAuth).Methods("POST")
-	r.HandleFunc("/api/register", HandleRegister).Methods("POST")
+	r.HandleFunc("/api/auth", SessionMiddleware(HandleAuth)).Methods("POST")
+	r.HandleFunc("/api/register", SessionMiddleware(HandleRegister)).Methods("POST")
 	fs := http.FileServer(http.Dir("static/"))
 
 	r.PathPrefix("/static").Handler(http.StripPrefix("/static/", fs))
