2eec0c8a87fd14488978cef1dd164a738cf906d4
ukhachev <ukhachev.v.s@gmail.com>
2019-03-08 17:03:19 +0300

    Cross platform support

--- server.go
+++ server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"path"
 
 	"github.com/gorilla/mux"
 )
@@ -21,16 +21,18 @@ func main() {
 	r.HandleFunc("/api/profile", SessionMiddleware(HandleUpdateUser, true)).Methods("PUT")
 	r.HandleFunc("/api/profile", SessionMiddleware(HandleGetUserData, true)).Methods("GET")
 	r.HandleFunc("/api/leaderbord/{page:[0-9]+}", SessionMiddleware(HandleGetUsers, true)).Methods("GET")
-	//staticServer := http.FileServer(http.Dir("../2019_1_DeathPacito_front/public"))
-	staticServer := http.FileServer(http.Dir("../2019_1_DeathPacito_front/public/"))
+
+	staticServer := http.FileServer(http.Dir(
+		path.Join("..", "2019_1_DeathPacito_front", "public")))
 	mediaServer := http.FileServer(http.Dir("media/"))
-	//r.PathPrefix("/public").Handler(http.StripPrefix("/public/", staticServer))
+
 	r.PathPrefix("/media").Handler(http.StripPrefix("/media/", mediaServer))
 	r.PathPrefix("/public").Handler(http.StripPrefix("/public/", staticServer))
 
 	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.URL.Path)
-		http.ServeFile(w, r, "../2019_1_DeathPacito_front/public/index.html")
+		http.ServeFile(w, r, path.Join(
+			"..", "2019_1_DeathPacito_front",
+			"public", "index.html"))
 	})
 
 	log.Fatal(http.ListenAndServe(":80", r))
